881535ea377a60c4e9699d9330c3d92be7ccefcd
Mark Erikson <mark@isquaredsoftware.com>
2021-09-03 16:55:06 -0400

    Use types for React 18 in TS setup

--- types/index.d.ts
+++ types/index.d.ts
@@ -5,3 +5,8 @@ declare module 'react-is' {
   export function isContextConsumer(value: any): value is React.ReactElement
   export function isValidElementType(value: any): value is React.ElementType
 }
+
+/* eslint-disable import/no-unresolved */
+import {} from 'react/next'
+
+import {} from 'react-dom/next'
